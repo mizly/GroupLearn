e0e5facb8660f707c4df8055a25074742c29091a
mizly <64050405+mizly@users.noreply.github.com>
2022-04-15 16:39:35 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+from requests.structures import CaseInsensitiveDict
+
+CLIENT_ID = "964586066252300308"
+CLIENT_SECRET = "XPHuUMzJBHx5jVw8BFMaFsZ_613NymYe"
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'lyonhacks 2022'
+def exchange_code(CLIENT_ID,CLIENT_SECRET,API_ENDPOINT,code):
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "code": code,
+        "grant_type": 'authorization_code',
+        "redirect_uri": "http://127.0.0.1:5000/auth?type=discord"
+    }
 
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    r = requests.post(API_ENDPOINT + '/oauth2/token', data=data, headers = headers)
+    r.raise_for_status()
+    return r.json()
+
+@app.route("/")
+def homepage():
+    return(render_template("auth.html"))
+
+@app.route("/auth")
+def authpage():
+    type = request.args.get('type')
+    if type == "discord":
+        code = request.args.get('code')
+        response = (exchange_code("964586066252300308","XPHuUMzJBHx5jVw8BFMaFsZ_613NymYe","https://discord.com/api/v8",code))
+
+        headers = CaseInsensitiveDict()
+        headers["Authorization"] = "Bearer " + response["access_token"]
+        resp = requests.get("https://discord.com/api/users/@me", headers=headers)
+        bruh = (resp.json())
+        return("You have been verified as %s#%s" % (bruh["username"],bruh["discriminator"]))
