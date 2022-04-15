e5d6878a0bc2061ec078664b1864e029b994c93b
cringemoment <69696899+cringemoment@users.noreply.github.com>
2022-04-15 14:45:14 -0400

    Add files via upload

--- messaging/app.py
+++ messaging/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+#main page
+@app.route("/")
+def testing():
+    return(render_template("index.html"))
+
+@app.route('/', methods=["POST"])
+def my_form_post():
+    username = request.form["text"]
+    message = request.form["message"]
+
+    with(open("static/messages.txt", "a") as messages):
+        if(username != "" and message != ""):
+            messages.write("%s: %s" % (username, message))
+            messages.write("\n")
+
+    return(render_template("index.html"))
