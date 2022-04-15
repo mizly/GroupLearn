81dcd5843c646ba8567efaaab31dd5b54076ce86
Abel Diress <abeldiress05@gmail.com>
2022-04-15 14:24:23 -0400

    added firebase module

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from requests import request
+import pyrebase
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+config = {
+    'apiKey': "AIzaSyDpMNHBRMH-4Rszj1GhDFU8tjCjfu7fvNQ",
+    'authDomain': "lyonhacks.firebaseapp.com",
+    'projectId': "lyonhacks",
+    'storageBucket': "lyonhacks.appspot.com",
+    'messagingSenderId': "852893010100",
+    'appId': "1:852893010100:web:0b18139976c45d6ae52fe1"
+}
+
+auth = pyrebase.initialize_app(config).auth()
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['name']
+        pwd = request.form['name']
+        pwd_confirm = request.form['name']
+
+        if pwd != pwd_confirm:
+            return render_template('signup.html', confirmFail=True)
+
+        
+
     return 'lyonhacks 2022'
 
 
