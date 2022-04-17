2a766a34e74cad9a58d77845bc1f9dbdfca9ef84
Abel Diress <abeldiress05@gmail.com>
2022-04-17 08:02:01 -0400

    matches page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, make_response, render_template, request, redirect, url_for, jsonify
+from sort import sort_compabitility
 from requests import HTTPError
 import pyrebase
 
@@ -11,35 +12,48 @@ config = {
     'storageBucket': "lyonhacks.appspot.com",
     'messagingSenderId': "852893010100",
     'appId': "1:852893010100:web:0b18139976c45d6ae52fe1",
-    'databaseURL': 'https://lyonhacks-default-rtdb.firebaseio.com'
+    'databaseURL': 'https://lyonhacks-default-rtdb.firebaseio.com',
+    'serviceAccount': 'lyonhacks-firebase-adminsdk-iqha5-753e386ab1.json'
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/verify')
 def verify():
+    if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '':
+        return redirect(url_for('login'))
+
+    email = request.cookies.get('email')
+    password = request.cookies.get('password')
+    user = auth.sign_in_with_email_and_password(email, password)
+    if not auth.get_account_info(user['idToken'])['users'][0]['emailVerified']:
+        auth.send_email_verification(user['idToken'])
+        return '<h1 style="font-family: Source Sans Pro;" align="center">Your account has yet to be verified. Check your email to verify this account.</h1>'
+    else:
+        return redirect(url_for('matches'))
+
+@app.route('/matches')
+def matches():
     # if user hasn't logged in yet, it redirects to login page
     try:
         session_id = request.cookies.get('ssid')
-
+            
         user = auth.sign_in_with_custom_token(session_id)
         accountInfo = auth.get_account_info(user['idToken'])
     except:
         return redirect(url_for('login'))
+            
+    user_info = list(db.child('/users/' + accountInfo['users'][0]['localId'] + '/').get(user['idToken']).val().values())[0]
     
-    email = accountInfo['users'][0]['email']
-    # checks if the email is not verified
-    if not accountInfo['users'][0]['emailVerified']:
-        auth.send_email_verification(user['idToken'])
-        return render_template('verify.html', email=email)
-
-    return redirect(url_for('notes'))
-
-@app.route('/matches')
-def matches():
-    return 'cum'
+    scores = sort_compabitility(user_info, db.child('users'))
+    
+    return jsonify(list(scores.values()))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -53,24 +67,22 @@ def signup():
         if pwd != pwd_confirm:
             return render_template('signup.html', confirmFail=True, weakPwd=False)
 
-        try:
-            user = auth.create_user_with_email_and_password(email, pwd)
-            auth.send_email_verification(user['idToken'])
+        #try:
+        user = auth.create_user_with_email_and_password(email, pwd)
 
-            data = {'name': name, 'grade': 0, 'subjects': [], 'day_available':[]}
-            
-            accountInfo = auth.get_account_info(user['idToken'])
-            db.child('/users/' + accountInfo['users'][0]['localId'] + '/').push(data, user['idToken'])
+        data = {'name': name, 'grade': 0, 'subjects': ['all'], 'day_available':[True, False]}
+        
+        accountInfo = auth.get_account_info(user['idToken'])
+        db.child('/users/' + accountInfo['users'][0]['localId'] + '/').push(data, user['idToken'])
 
-            session_id = auth.create_custom_token(accountInfo['users'][0]['localId'])
-
-            resp = make_response(redirect(url_for('verify')))
-            resp.set_cookie('ssid', session_id)
-            return resp
-        except Exception as err:
-            print(err)
-            if 'WEAK_PASSWORD' in str(err):
-                return render_template('signup.html', confirmFail=False, weakPwd=True)
+        resp = make_response(redirect(url_for('verify')))
+        resp.set_cookie('email', email)
+        resp.set_cookie('password', pwd)
+        return resp
+        # except Exception as err:
+        #     print(err)
+        #     if 'WEAK_PASSWORD' in str(err):
+        #         return render_template('signup.html', confirmFail=False, weakPwd=True)
 
     return render_template('signup.html', confirmFail=False, weakPwd=False)
 
@@ -82,12 +94,10 @@ def login():
         try:
             user = auth.sign_in_with_email_and_password(email, pwd)
             accountInfo = auth.get_account_info(user['idToken'])
-            userInfoId = list(db.child('/users/' + accountInfo['users'][0]['localId'] + '/').get(user['idToken']).val())[0]
-
-            session_id = auth.create_custom_token(accountInfo['users'][0]['localId'])
 
             resp = make_response(redirect(url_for('verify')))
-            resp.set_cookie('ssid', session_id)
+            resp.set_cookie('email', email)
+            resp.set_cookie('password', password)
             return resp
         except HTTPError as e:
             e = str(e)
@@ -100,15 +110,11 @@ def login():
                     
         return redirect(url_for('verify'))
         
-    # if user hasn't logged in yet, it redirects to login page
     try:
-        session_id = request.cookies.get('ssid')
-
-        user = auth.sign_in_with_custom_token(session_id)
-        accountInfo = auth.get_account_info(user['idToken'])
-        return redirect(url_for('verify'))
-    except Exception as e:
-        print(e)
+        email = request.cookies.get('email')
+        password = request.cookies.get('password')
+        user = auth.sign_in_with_email_and_password(email, password)
+    except:
         return render_template('login.html', invalidPwd=False)
 
 if __name__ == '__main__':
---
+++ sort.py
@@ -0,0 +1,48 @@
+#data structure; paralell between user dict and profile's dict values
+# user = {
+#     "grade": int
+#     "subjects": ["array", "of", "strings"]
+#     "day_available": [
+#         [True, False, True, False],
+#         [False, True, False, True],
+#         [True, False, True, False]
+#     ] 2D array containing arrays of bools, representing hours available
+
+def sort_compabitility(user, **profile): #user and profile are for the target and comparisons, respectively
+    comp_score = {} #stores compatibilty scores for each profile
+
+    for key in profile: #parses through all the profiles
+
+        localId = list(profile[key].keys())[0]
+        profile[key] = profile[key][localId]
+
+        shared_subjects = 0 #for tracking subjects shared
+        shared_times = 0
+
+        if dif := abs( user["grade"] - profile[key]["grade"] ) > 2: #assign dif variable as difference in grade, also checks if it's greater than 2
+            profile.pop(key) #removes people who are more than 2 grades apart
+            continue
+
+        for i in user["subjects"]: #should be parsing through subjects array of user
+            try:
+                profile[key]["subjects"].index(i) #we do a little try-catching
+                shared_subjects += 1
+            except:
+                pass #.index() returns ValueError if not found, meaning it skips the increment part
+        
+        if shared_subjects == 0:
+            profile.pop(key) #removes the people who don't share a subject
+            continue
+
+        for i in range(7): #hardcode lol, original was range(len( user["day_available"] ))
+            for j in range(24): #range(len( user["day_available"][i] ))
+                if user["day_available"][i][j] and profile[key]["day_available"][i][j]: #great addresses; checks for if both people have matching times
+                    shared_times += 1
+        
+        if shared_times == 0:
+            profile.pop(key) #removes the people who don't share a time slot
+            continue
+
+        comp_score[key] = 1000*dif - 100*shared_subjects - shared_times #assigns score to profile based on grade difference; the lower the better
+
+    return comp_score #or something, in this state it's returning a dictionary with the same key but int values for compatibility scores
